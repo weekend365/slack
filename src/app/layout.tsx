fa1a7ee82129a14366d0aad46097f064071240cf
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2025-01-18 17:23:00 +0900

    Database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
+// "use client";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
