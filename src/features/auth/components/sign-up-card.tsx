b3cb2c2f42d86fedc4b45bbbc6ecdebac50c8417
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2025-01-21 13:54:21 +0900

    User information

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -22,6 +22,7 @@ interface SignUpCardProps {
 function SignUpCard({ setState }: SignUpCardProps) {
   const { signIn } = useAuthActions();
 
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -37,7 +38,7 @@ function SignUpCard({ setState }: SignUpCardProps) {
     }
 
     setPending(true);
-    signIn("password", { email, password, flow: "signUp" })
+    signIn("password", { name, email, password, flow: "signUp" })
       .catch(() => {
         setError("Something went wrong");
       })
@@ -69,6 +70,15 @@ function SignUpCard({ setState }: SignUpCardProps) {
       )}
       <CardContent className="space-y-5 px-0 pb-0">
         <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+          <Input
+            disabled={pending}
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Full name"
+            required
+          ></Input>
           <Input
             disabled={pending}
             value={email}
