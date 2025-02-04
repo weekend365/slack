39385a0299702bec92dd03e6994208ca7a460456
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2025-02-04 09:38:08 +0900

    Channel header component

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -0,0 +1,176 @@
+import { Button } from "@/components/ui/button";
+import { FaChevronDown } from "react-icons/fa";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useUpdateChannel } from "@/features/channels/api/use-update.channel";
+import { toast } from "sonner";
+import { useRemoveChannel } from "@/features/channels/api/use-remove.channel";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { UseCurrentMember } from "@/features/members/api/use-current-member";
+
+interface HeaderProps {
+  title: string;
+}
+export const Header = ({ title }: HeaderProps) => {
+  const router = useRouter();
+  const channelId = useChannelId();
+  const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete this Channel?",
+    "You are about to delete this channel. This action is irreversible"
+  );
+
+  const [value, setValue] = useState(title);
+  const [editOpen, setEditOpen] = useState(false);
+
+  const { data: member } = UseCurrentMember({ workspaceId });
+  const { mutate: updateChannel, isPending: isUpdatingChannel } =
+    useUpdateChannel();
+  const { mutate: removeChannel, isPending: isRemovingChannel } =
+    useRemoveChannel();
+
+  const handleEditOpen = (value: boolean) => {
+    if (member?.role !== "admin") return;
+
+    setEditOpen(value);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setValue(value);
+  };
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (!ok) {
+      return;
+    }
+
+    removeChannel(
+      { id: channelId },
+      {
+        onSuccess: () => {
+          toast.success("Channel deleted successfully");
+          router.push(`/workspace/${workspaceId}`);
+        },
+        onError: () => {
+          toast.error("Failed to delete channel");
+        },
+      }
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateChannel(
+      { id: channelId, name: value },
+      {
+        onSuccess: () => {
+          toast.success("channel updated successfully");
+          setEditOpen(false);
+        },
+        onError: () => {
+          toast.error("Failed to update channel");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden">
+      <ConfirmDialog />
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="text-lg font-semibold px-2 overflow-hidden w-auto"
+            size="sm"
+          >
+            <span className="truncate"># {title}</span>
+            <FaChevronDown
+              style={{
+                width: "0.75rem",
+                height: "0.75rem",
+                marginLeft: "0.5rem",
+              }}
+            />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle># {title}</DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <Dialog open={editOpen} onOpenChange={handleEditOpen}>
+              <DialogTrigger asChild>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Channel name</p>
+                    {member?.role === "admin" && (
+                      <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                        Edit
+                      </p>
+                    )}
+                  </div>
+                  <p className="text-sm"># {title}</p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this channel</DialogTitle>
+                </DialogHeader>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <Input
+                    value={value}
+                    disabled={isUpdatingChannel}
+                    onChange={handleChange}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="e.g. plan-budget"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline" disabled={isUpdatingChannel}>
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button disabled={isUpdatingChannel}>Save</Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+            {member?.role === "admin" && (
+              <button
+                onClick={handleDelete}
+                disabled={isRemovingChannel}
+                className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600"
+              >
+                <TrashIcon className="size-4" />
+                <p className="text-sm font-semibold">Delete channel</p>
+              </button>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
